a92ed1b6f7b12507c44cef75a5f752ac861a5c1e
Trollerr <thanhdado480@gmail.com>
2020-04-26 11:17:30 +0700

    dmBot

--- bot.py
+++ bot.py
@@ -7,4 +7,16 @@ client = discord.Client()
 async def on_ready():
     print("Sẵn sàng hoạt động!")
 
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    else:
+        if "uwu" in message.content.lower():
+            await message.channel.send("UwU")
+        if "owo" in message.content.lower():
+            await message.channel.send("OwO")
+        if "chào" in message.content.lower():
+            await message.channel.send("Chào bạn tui!")
+
 client.run(os.environ['DITMEMAY'])
