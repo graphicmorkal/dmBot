1cf318d95877ac92a499030af7596b6c6d01cbb7
Trollerr <thanhdado480@gmail.com>
2020-04-28 18:19:36 +0700

    no

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ async def on_message(message):
                 await message.channel.delete_messages(msg)
                 await message.channel.send(f"{message.author.mention} đã xoá %s tin nhắn!" % con[1])
         if message.content.startswith('t!aolang'):
-            await message.channel.send('Chưa có thông tin gì về giải ao làng sắp tới, nên cứ hóng đi nhé :)')
+            await message.channel.send(embed = 'Chưa có thông tin gì về giải ao làng sắp tới, nên cứ hóng đi nhé :)')
 
 bot = commands.Bot(command_prefix='t!')
 
