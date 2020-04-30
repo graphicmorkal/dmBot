868dc63d226609980572f7caa225e927652f3f72
Trollerr <thanhdado480@gmail.com>
2020-04-30 11:03:58 +0700

    5

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ async def on_message(message):
                     if status == 'info':
                         await message.channel.send('**Giải Ao Làng Đông Lào lần thứ 3**\n*Thời gian tổ chức:* từ **5/5** đến **7/5**  \n__Giải được chia làm 2 bảng đấu:__\n - Bảng vinh quang: Dành cho những người rank Kin cương trở xuống\n - Bảng siêu sao: Dành cho những người Rank master và người vào chung kết trong bảng vinh quang.\n*Cơ cấu giải thưởng:* 10 chai sting dành cho vô địch bảng (10k/chai)')
                     if status == "thamgia":
-                        await message.channel.send('Đăng ký ngay ở #đăng-kí theo mẫu sau:\n*Tên discord*    |   *Tên trong game*  |  *Rank*\nTroller#4495 | WjbuLord#7749 | Bạc 3\n(Link)[https://discordapp.com/channels/695461977438421004/705012199277461505/705017959709409280]')
+                        await message.channel.send('{message.author.mention} nên đăng ký ngay ở #đăng-kí theo mẫu sau:\n*Tên discord*    |   *Tên trong game*  |  *Rank*\nTroller#4495 | WjbuLord#7749 | Bạc 3\n(Link)[https://discordapp.com/channels/695461977438421004/705012199277461505/705017959709409280]')
         if message.content.startswith('t!help'):
             ctx = message.content.split()
             if (len(ctx) < 2):
