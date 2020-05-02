81b3214b42d6c850d2df3c6059f53f236c40cb17
Trollerr <thanhdado480@gmail.com>
2020-05-02 15:23:18 +0700

    12

--- bot.py
+++ bot.py
@@ -44,12 +44,12 @@ async def on_message(message):
         # Check nếu là admin (chạy code ở dưới)
         if message.content.startswith('t!clear'):
             con = message.content.split()
+            await message.channel.send("Ai cho bác cái quyền để xóa tin nhắn vậy?")
+            if authorIsAdmin(message):
                 msg = []
                 lists = await message.channel.history(limit=int(con[1])+1).flatten()
                 for x in lists:
                     msg.append(x)
-            await message.channel.send("Ai cho bác cái quyền để xóa %s tin nhắn vậy?" % con[1])
-            if authorIsAdmin(message):
                 await message.channel.delete_messages(msg)
                 await message.channel.send(f"À nhầm, xin lỗi {message.author.mention} nhé! Xoá %s tin nhắn rồi đấy!" % con[1])
         if message.content.startswith('t!aolang'):
