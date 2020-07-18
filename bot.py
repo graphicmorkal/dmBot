965ad33ec705da5fa887f1d4de813b65315708f2
Trollerr <thanhdado480@gmail.com>
2020-07-18 21:58:37 +0700

    update 6

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ async def on_message(message):
             time.sleep(1)
             if authorIsAdmin(message):
                 await message.channel.send("Mà khoan, bác có **quyền lực** rồi mà?")
-        if "CHXHCNVN" in message.content.lower():
+        if "chxhcnvn" in message.content.lower():
             await message.channel.send("Nhóm này đã bị điều tra bởi các cơ quan trực thuộc Bộ Công an (và/hoặc các tổ chức chính trị tương tự phục vụ cho nhà nước CHXHCNVN).")
             await message.channel.send("Cơ quan điều tra (CQĐT) khẳng định các thành viên trong này liên quan tới nhóm hoặc những cá nhân khác trong đây. CQĐT biết rõ tại sao các thành viên lại có mặt ở đây vào thời điểm này, tài khoản của họ không được thêm bởi một bên thứ ba mà chính họ tự tham gia vào đây qua đường dẫn của (những) người đứng đầu. Các nhân viên của CQĐT cũng xin khẳng định rằng những hành động chống phá Đảng và Nhà nước đã được thực hiện bởi các thành viên trong nhóm này.")
             time.sleep(3)
